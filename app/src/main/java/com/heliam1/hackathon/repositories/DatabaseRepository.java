4d56210d643bfae64165bf6f3dd9c87a718c49ad
heliam1 <liamjtb@gmail.com>
2018-04-22 02:14:38 +1000

    implements repository for user and group[

--- app/src/main/java/com/heliam1/hackathon/repositories/DatabaseRepository.java
+++ app/src/main/java/com/heliam1/hackathon/repositories/DatabaseRepository.java
@@ -5,7 +5,9 @@ import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.location.Location;
 import android.net.Uri;
+import android.util.Log;
 
 import com.heliam1.hackathon.models.Group;
 import com.heliam1.hackathon.models.User;
@@ -14,6 +16,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Single;
+
+import com.heliam1.hackathon.data.HackathonContract.GroupEntry;
+import com.heliam1.hackathon.data.HackathonContract.UserEntry;
 
 public class DatabaseRepository implements GroupsRepository, UserRepository {
     private final ContentResolver contentResolver;
@@ -66,137 +71,141 @@ public class DatabaseRepository implements GroupsRepository, UserRepository {
         });
     }
 
-    private List<Workout> queryWorkouts() {
+    private List<Group> queryGroups() {
         String[] projection = {
-                WorkoutEntry._ID,
-                WorkoutEntry.COLUMN_WORKOUT_NAME,
-                WorkoutEntry.COLUMN_WORKOUT_IMAGE,
-                WorkoutEntry.COLUMN_WORKOUT_LAST_DATE_COMPLETED,
-                WorkoutEntry.COLUMN_WORKOUT_DURATION};
+                GroupEntry._ID,
+                GroupEntry.COLUMN_GROUP_NAME,
+                GroupEntry.COLUMN_GROUP_SUBJECT_CODE,
+                GroupEntry.COLUMN_GROUP_USER_COUNT,
+                GroupEntry.COLUMN_GROUP_LOCATION,
+                GroupEntry.COLUMN_GROUP_RATING};
 
-        String sortOrder = WorkoutEntry._ID;
+        Cursor cursor = contentResolver.query(GroupEntry.CONTENT_URI,
+                projection, null, null, null);
 
-        Cursor cursor = contentResolver.query(ExerciseSetEntry.CONTENT_URI,
-                projection, null, null, sortOrder);
-
-        List<Workout> workouts = new ArrayList<Workout>();
+        List<Group> groups = new ArrayList<Group>();
 
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
-            workouts.add(new Workout(
-                    cursor.getLong(cursor.getColumnIndex(WorkoutEntry._ID)),
-                    cursor.getString(cursor.getColumnIndex(WorkoutEntry.COLUMN_WORKOUT_NAME)),
-                    cursor.getInt(cursor.getColumnIndex(WorkoutEntry.COLUMN_WORKOUT_IMAGE)),
-                    cursor.getString(cursor.getColumnIndex(
-                            WorkoutEntry.COLUMN_WORKOUT_LAST_DATE_COMPLETED)),
-                    cursor.getInt(cursor.getColumnIndex(WorkoutEntry.COLUMN_WORKOUT_DURATION))));
+            groups.add(new Group(
+                    cursor.getLong(cursor.getColumnIndex(GroupEntry._ID)),
+                    new Location(cursor.getString(cursor.getColumnIndex(GroupEntry.COLUMN_GROUP_LOCATION))),
+                    cursor.getString(cursor.getColumnIndex(GroupEntry.COLUMN_GROUP_NAME)),
+                    cursor.getInt(cursor.getColumnIndex(GroupEntry.COLUMN_GROUP_SUBJECT_CODE)),
+                    cursor.getInt(cursor.getColumnIndex(GroupEntry.COLUMN_GROUP_USER_COUNT)),
+                    cursor.getInt(cursor.getColumnIndex(GroupEntry.COLUMN_GROUP_RATING))));
             cursor.moveToNext();
         }
         cursor.close();
 
-        return workouts;
+        return groups;
     }
 
-    private List<ExerciseSet> queryExerciseSetsByWorkoutId(long workoutId) {
+    private List<User> queryUser() {
         String[] projection = {
-                ExerciseSetEntry._ID,
-                ExerciseSetEntry._WORKOUT_ID,
-                ExerciseSetEntry.COLUMN_EXERCISE_NAME,
-                ExerciseSetEntry.COLUMN_SET_NUMBER,
-                ExerciseSetEntry.COLUMN_SET_DURATION,
-                ExerciseSetEntry.COLUMN_SET_REST,
-                ExerciseSetEntry.COLUMN_SET_WEIGHT,
-                ExerciseSetEntry.COLUMN_SET_REPS,
-                ExerciseSetEntry.COLUMN_SET_DATE,
-                ExerciseSetEntry.COLUMN_SET_ORDER,
-                ExerciseSetEntry.COLUMN_PB_WEIGHT,
-                ExerciseSetEntry.COLUMN_PB_REPS};
+                UserEntry._ID_STUDENT,
+                UserEntry.COLUMN_USER_PSEUDONAME,
+                UserEntry.COLUMN_USER_LOCATION};
 
-        String selection = ExerciseSetEntry._WORKOUT_ID + "=?";
+        Cursor cursor = contentResolver.query(UserEntry.CONTENT_URI,
+                projection, null, null, null);
 
-        String[] selectionArgs = {Long.toString(workoutId)};
-
-        // String sortOrder = ExerciseSetEntry.COLUMN_SET_DATE + " DESC";
-        String sortOrder = ExerciseSetEntry.COLUMN_SET_ORDER + " ASC";
-
-        Cursor cursor = contentResolver.query(ExerciseSetEntry.CONTENT_URI,
-                projection, selection, selectionArgs, sortOrder);
-
-        List<ExerciseSet> exerciseSets = new ArrayList<ExerciseSet>();
+        List<User> users = new ArrayList<User>();
 
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
-            exerciseSets.add(new ExerciseSet(
-                    cursor.getLong(cursor.getColumnIndex(ExerciseSetEntry._ID)),
-                    cursor.getLong(cursor.getColumnIndex(ExerciseSetEntry._WORKOUT_ID)),
-                    cursor.getString(cursor.getColumnIndex(ExerciseSetEntry.COLUMN_EXERCISE_NAME)),
-                    cursor.getInt(cursor.getColumnIndex(ExerciseSetEntry.COLUMN_SET_NUMBER)),
-                    cursor.getInt(cursor.getColumnIndex(ExerciseSetEntry.COLUMN_SET_DURATION)),
-                    cursor.getInt(cursor.getColumnIndex(ExerciseSetEntry.COLUMN_SET_REST)),
-                    cursor.getDouble(cursor.getColumnIndex(ExerciseSetEntry.COLUMN_SET_WEIGHT)),
-                    cursor.getInt(cursor.getColumnIndex(ExerciseSetEntry.COLUMN_SET_REPS)),
-                    cursor.getString(cursor.getColumnIndex(ExerciseSetEntry.COLUMN_SET_DATE)),
-                    cursor.getInt(cursor.getColumnIndex(ExerciseSetEntry.COLUMN_SET_ORDER)),
-                    cursor.getInt(cursor.getColumnIndex(ExerciseSetEntry.COLUMN_PB_WEIGHT)),
-                    cursor.getInt(cursor.getColumnIndex(ExerciseSetEntry.COLUMN_PB_REPS))));
+            users.add(new User(
+                    cursor.getLong(cursor.getColumnIndex(UserEntry._ID_STUDENT)),
+                    cursor.getString(cursor.getColumnIndex(UserEntry.COLUMN_USER_PSEUDONAME)),
+                    new Location(cursor.getString(cursor.getColumnIndex(UserEntry.COLUMN_USER_LOCATION)))));
 
             cursor.moveToNext();
         }
         cursor.close();
 
-        return exerciseSets;
+        return users;
     }
 
-    private Long upsertWorkout(Workout workout) {
+    private Long upsertGroup(Group group) {
         ContentValues values = new ContentValues();
-        values.put(WorkoutEntry.COLUMN_WORKOUT_NAME, workout.getName());
-        values.put(WorkoutEntry.COLUMN_WORKOUT_IMAGE, workout.getImage());
-        values.put(WorkoutEntry.COLUMN_WORKOUT_LAST_DATE_COMPLETED, workout.getDate());
-        values.put(WorkoutEntry.COLUMN_WORKOUT_DURATION, workout.getDuration());
+        values.put(GroupEntry.COLUMN_GROUP_NAME, group.getName());
+        values.put(GroupEntry.COLUMN_GROUP_SUBJECT_CODE, group.getSubjectCode());
+        values.put(GroupEntry.COLUMN_GROUP_USER_COUNT, group.getUserCount());
+        values.put(GroupEntry.COLUMN_GROUP_LOCATION, group.getLocation().toString());
+        values.put(GroupEntry.COLUMN_GROUP_RATING, group.getRating());
 
         // uri or Long savedWorkoutId; deffs want long but convert from uri?
         Uri uri;
 
-        if (workout.hasId()) {
+        if (group.hasId()) {
             contentResolver.update(
-                    ContentUris.withAppendedId(WorkoutEntry.CONTENT_URI, workout.getId()),
+                    ContentUris.withAppendedId(GroupEntry.CONTENT_URI, group.getId()),
                     values, null, null);
 
             // TODO:
-            uri = ContentUris.withAppendedId(WorkoutEntry.CONTENT_URI, workout.getId());
+            uri = ContentUris.withAppendedId(GroupEntry.CONTENT_URI, group.getId());
         } else {
-            uri = contentResolver.insert(WorkoutEntry.CONTENT_URI, values);
+            uri = contentResolver.insert(GroupEntry.CONTENT_URI, values);
         }
         return parseUriToId(uri);
     }
 
-    private Long upsertExerciseSet(ExerciseSet exerciseSet) {
+    private Long upsertUser(User user) {
         ContentValues values = new ContentValues();
-        values.put(ExerciseSetEntry._WORKOUT_ID, exerciseSet.getWorkoutId());
-        values.put(ExerciseSetEntry.COLUMN_EXERCISE_NAME, exerciseSet.getExerciseName());
-        values.put(ExerciseSetEntry.COLUMN_SET_NUMBER, exerciseSet.getSetNumber());
-        values.put(ExerciseSetEntry.COLUMN_SET_DURATION, exerciseSet.getSetDuration());
-        values.put(ExerciseSetEntry.COLUMN_SET_REST, exerciseSet.getSetRest());
-        values.put(ExerciseSetEntry.COLUMN_SET_WEIGHT, exerciseSet.getSetWeight());
-        values.put(ExerciseSetEntry.COLUMN_SET_REPS, exerciseSet.getSetReps());
-        values.put(ExerciseSetEntry.COLUMN_SET_DATE, exerciseSet.getSetDate());
-        values.put(ExerciseSetEntry.COLUMN_SET_ORDER, exerciseSet.getSetOrder());
-        values.put(ExerciseSetEntry.COLUMN_PB_WEIGHT, exerciseSet.getPbWeight());
-        values.put(ExerciseSetEntry.COLUMN_PB_REPS, exerciseSet.getPbReps());
+        values.put(UserEntry.COLUMN_USER_PSEUDONAME, user.getPseudoname());
+        values.put(UserEntry.COLUMN_USER_LOCATION, user.getLocation().toString());
 
         // uri or Long savedWorkoutId; deffs want long but convert from uri?
         Uri uri;
 
-        if (exerciseSet.hasId()) {
+        if (user.hasId()) {
             contentResolver.update(
-                    ContentUris.withAppendedId(ExerciseSetEntry.CONTENT_URI, exerciseSet.getId()),
+                    ContentUris.withAppendedId(UserEntry.CONTENT_URI, user.getStudentId()),
                     values, null, null);
 
             // TODO:
-            uri = ContentUris.withAppendedId(ExerciseSetEntry.CONTENT_URI, exerciseSet.getId());
+            uri = ContentUris.withAppendedId(UserEntry.CONTENT_URI, user.getStudentId());
         } else {
-            uri = contentResolver.insert(ExerciseSetEntry.CONTENT_URI, values);
+            uri = contentResolver.insert(UserEntry.CONTENT_URI, values);
         }
         return parseUriToId(uri);
+    }
+
+    @Override
+    public Single<Long> deleteGroup(Group group) {
+        return Single.fromCallable(() -> {
+            try {
+                Long id  = group.getId();
+                contentResolver.delete(
+                        ContentUris.withAppendedId(GroupEntry.CONTENT_URI, group.getId()),
+                        null, null);
+                return id;
+            } catch (Exception e) {
+                throw new RuntimeException("Something wrong with db");
+            }
+        });
+    }
+
+    @Override
+    public Single<Long> deleteUser(User user) {
+        return Single.fromCallable(() -> {
+            try {
+                Long id  = user.getStudentId();
+                contentResolver.delete(
+                        ContentUris.withAppendedId(UserEntry.CONTENT_URI, id),
+                        null, null);
+                return id;
+            } catch (Exception e) {
+                throw new RuntimeException("Something wrong with db");
+            }
+        });
+    }
+
+    Long parseUriToId(Uri uri) {
+        String idString = uri.toString();
+        idString = idString.replaceFirst("1","");
+        String idValueString = idString.replaceAll("[^0-9]", "");
+        Log.v("Task:", "extracted long id: " + idValueString);
+        return Long.parseLong(idValueString);
     }
 }
--- app/src/main/java/com/heliam1/hackathon/repositories/UserRepository.java
+++ app/src/main/java/com/heliam1/hackathon/repositories/UserRepository.java
@@ -1,4 +1,17 @@
 package com.heliam1.hackathon.repositories;
 
+import com.heliam1.hackathon.models.User;
+
+import java.util.List;
+
+import io.reactivex.Single;
+
 public interface UserRepository {
+    public Single<List<User>> getUser();
+
+    public Single<Long> saveUser(User user);
+
+    public Single<Long> deleteUser(User user);
+
+    // Single<List<Task>> searchTasks(String title);
 }
