15efc52eab661a73c1fd780d16d9e2c35c1583d4
heliam1 <liamjtb@gmail.com>
2018-04-22 07:04:53 +1000

    implement groupmessage repostiory, possible mistakes cos tired

--- app/src/main/java/com/heliam1/hackathon/models/GroupMessage.java
+++ app/src/main/java/com/heliam1/hackathon/models/GroupMessage.java
@@ -1,19 +1,47 @@
 package com.heliam1.hackathon.models;
 
 public class GroupMessage {
-    long id;
+    Long id;
     long groupId;
     long userId;
-    long userName;
+    String userName;
     String zonedDateTime;
     String messageText;
 
-    public GroupMessage(long id, long groupId, long userId, long userName, String zonedDateTime, String messageText) {
+    public GroupMessage(long id, long groupId, long userId, String userName, String zonedDateTime, String messageText) {
         this.id = id;
         this.groupId = groupId;
         this.userId = userId;
         this.userName = userName;
         this.zonedDateTime = zonedDateTime;
         this.messageText = messageText;
+    }
+
+    public boolean hasId() {
+        return (this.id != null);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getZonedDateTime() {
+        return zonedDateTime;
+    }
+
+    public String getMessageText() {
+        return messageText;
     }
 }
